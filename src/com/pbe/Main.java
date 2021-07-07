b3857e4c6550a337a73a2085238fbc52fab61456
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2021-07-07 11:42:24 +0200

    complete multithreading topic for now

--- src/com/pbe/Main.java
+++ src/com/pbe/Main.java
@@ -11,14 +11,20 @@ package com.pbe;
 // *********************
 // A multithreaded program consists of two or more parts that run concurrently.
 // Each part is called a thread. Each thread defines its own path of execution.
-// I.e. it can be considered a form of multitasking. Two distinct types of multitasking:
+// A thread is an executable, lightweight unit that accesses shared resources as well as its own call stack.
+
+// Threads can be considered a form of multitasking. Two distinct types of multitasking:
 // 1. Process-based: Having two or more programs run simultaneously.
-// 2. Thread-based: Having two or more 'tasks' within a program run simultaneously.
+// 2. Thread-based: Having two or more threads (read: tasks) within a program run simultaneously.
 // With process-based multitasking a program is the smallest unit of code and with thread-based, a thread.
 
 // Multitasking threads requires less overhead them multitasking processes (which btw are out of control of Java), as threads share common resources.
 // Multithreading make it possible for application to maximize use of processing power of the system.
 // It does so by minimizing idle time, as multiple threads can run while others are still waiting for example on data or input.
+
+// Other terms used in relation with multithreading:
+// Multiprocessing: more than one process is executed simultaneously. Similar to multitasking, but here more than one CPU is involved.
+// Parallel processing: a technique where multiple CPUs work simultaneously in a computer system (see Fork/Join framework in Java for more on this).
 
 // Thread model
 // Java run-time uses threads to enable the entire environment to be asynchronous.
@@ -38,21 +44,6 @@ package com.pbe;
 // 5. Blocked, when waiting for a resource
 // And at any time, a thread can be terminated. Once terminated, the thread no longer exists.
 
-// Thread priorities
-// Threads are assigned a priority (integer value) that determines how each thread should be treated compared to others.
-// Switching from one thread to another is called a context switch. A contact switch takes place by the following rules:
-// 1. Voluntarily relinquish control by a thread: This occurs when explicitly yielding, sleeping or when blocked. CPU is given to the highest prio thread that's ready.
-// 2. Preemptive multitasking: Lower prio thread that does not yield the CPU is preempted. Higher prio thread that wants to run, does so.
-// How prioritization of multiple threads with equal priority that compete for CPU cycles is resolved, depends on the operating system.
-
-// Synchronization
-// Multithreading introduces asynchronous behavior to programs. There are ways to enforce synchronicity when needed.
-// This is needed for example to prevent conflicts when two threads are required to communicate and share a complicated data structure.
-// For example a linked list, where one thread shouldn't be writing, while the other is still reading.
-// Java uses 'the monitor' for this purpose. This is a control system that can be used to protect a shared asset from being manipulated by more than one thread at the time.
-// Each object has its own implicit monitor that's automatically entered when one of the objects synchronized methods is called.
-// When a thread is inside a synchronized method, no other thread can call any other synchronized method on the same object.
-
 // Messaging
 // With a program divided in separate threads, it must be defined how they communicate with each other.
 // Java's messaging system allows a thread to enter a synchronized method on an object, and then wait until some other thread explicitly notifies it to come out.
@@ -69,6 +60,8 @@ package com.pbe;
 // The main thread can be controlled through a Thread object, by obtaining a reference to it with currentThread(), a public static member of Thread.
 // The method returns a reference to the thread in which it is called. Once referenced, it can be controlled like any other thread.
 // Threads are part of groups. A thread group is a data structure that controls the state of a collection of threads as a whole.
+
+// Note: threads that are mainly used in the background are sometimes also referred to as 'daemon threads'.
 
 // Creating a thread
 // A thread can be created by instantiating an object of type Thread:
@@ -107,13 +100,24 @@ package com.pbe;
 // It also allows to specify a maximum amount of time to wait for the specified thread to terminate.
 
 // Thread priorities
+// The Thread class provides a setPriority() method to set the priority for a thread.
+// Threads are assigned a priority (integer value) that determines how each thread should be treated compared to others.
+// Using thread priority, it can be decide when to switch from one thread in running state to another.
+// Switching from one thread to another is called a 'context switch' or 'context switching', the process wherein the program switches contexts of the threads.
+// A contact switch takes place by the following rules:
+// 1. Voluntarily relinquish control by a thread: This occurs when explicitly yielding, sleeping or when blocked. CPU is given to the highest prio thread that's ready.
+// 2. Preemptive multitasking: Lower prio thread that does not yield the CPU is preempted. Higher prio thread that wants to run, does so.
+//
 // The thread scheduler uses priorities to decide when threads should be allowed to run.
-// The amount of CPU time given to a thread often depends on factors besides priority, such how the OS implements multitasking.
+// How prioritization of multiple threads with equal priority that compete for CPU cycles is resolved, depends on the operating system.
+// Also the amount of CPU time given to a thread often depends on factors besides priority, such how the OS implements multitasking.
+//
 // The safest way to obtain predictable, cross-platform behavior with Java is to use threads that voluntary yield control.
 // A higher priority thread can preempt (=take action to prevent an anticipated event happening) a lower priority thread.
 // For safety, threads that share the same priority should yield control once in a while.
 // This to ensure all threads have a chance to run under a non-preemptive OS (= non interrupting a process running CPU in the middle of execution).
 // Threads of CPU-intensive tasks dominate the CPU and for this reason control should be yielded occasionally so that other threads can run.
+//
 // Use setPriority() to set a thread's priority: final void setPriority(int level) - with level being the priority setting.
 // The value of level must be in the range MIN_PRIORITY (=1) and MAX_PRIORITY (=10)
 // NORM_PRIORITY is used to return a thread to default priority (=5).
@@ -121,9 +125,14 @@ package com.pbe;
 // Obtain the current priority setting by calling getPriority(): final int getPriority()
 
 // Synchronization
+// Multithreading introduces asynchronous behavior to programs. There are ways to enforce synchronicity when needed.
+// This is needed for example to prevent conflicts when two threads are required to communicate and share a complicated data structure.
+// For example a linked list, where one thread shouldn't be writing, while the other is still reading.
 // Synchronization is the process to ensure that a resource is used only by a single thread at a time.
 // Synchronization uses a 'monitor' to achieve this: an object that is used as a mutually exclusive lock.
+// Each object has its own implicit monitor that's automatically entered when one of the objects synchronized methods is called.
 // Each thread can 'own' a monitor at a given time and is said to have 'entered' the monitor when acquiring a lock.
+// When a thread is inside a synchronized method, no other thread can call any other synchronized method on the same object.
 // Other threads attempting to enter the locked monitor will be suspended until the initial thread 'exits' the monitor.
 // The other threads are 'waiting'.
 // A thread that owns a monitor can reenter it if it wants.
@@ -132,13 +141,12 @@ package com.pbe;
 // 1. Using synchronized methods
 // 2. The synchronized statement
 // ->>
-
 // 1. Using synchronized methods
 // All objects have their own implicit monitor associated with them.
 // To enter an objects monitor, make a call to a method that has been modified with the synchronized keyword.
 // Threads that call a synchronized method on the same instance while there's already a thread inside, must wait.
 // The monitor is exited by returning from the synchronized method and can then relinquish control of the object to the next waiting thread.
-
+//
 // 2. The synchronized statement
 // Creating synchronized methods within classes to achieve synchronization will not work in all cases.
 // For example when using a third party class that does not use synchronized methods and does not offer access to its source code.
@@ -175,12 +183,25 @@ package com.pbe;
 
 // Suspending, Resuming and Stopping Threads
 // Suspending execution of a thread can be useful sometimes. In early version of Java programs could use suspend(), resume(), stop().
-// Which are methods defined by Thread to control the execution of a thread. These method should/can no longer be used.
+// Which are methods defined by Thread to control the execution of a thread. These method should/can no longer be used, for safety reasons.
 // Instead, a thread must be designed so that the run() method periodically checks to determine whether a thread should suspend/resume/stop.
 // This can be accomplished by establishing a flag variable that indicates the execution state of the thread.
 // The flag can be set to 'running', 'suspend' or 'stop' to have the thread respectively continue, suspend or stop.
 // This approach must be used for all new code.
 
+// Obtaining a thread's state
+// A thread can exist in a number of different states. The current state can be obtained by calling getState(): Thread.State getState()
+// It returns a value of type Thread.State, which indicates the state of the thread at the time of the call.
+// State is an enumeration (=list of named constants) defined by Thread.
+// Values that can be returned by getState(): BLOCKED, NEW, RUNNABLE, TERMINATED, TIMED_WAITING, WAITING
+// To determine for example if a thread name t is in the RUNNABLE state:
+// Thread.State ts = t.getState()
+// if(ts == Thread.State.RUNNABLE) // ...
+// Note: a thread's state may (a moment later) change after the call to getState()! Use it primarily for debugging or for profiling a thread's run-time characteristics.
+
+// Using a factory method to create and start a thread
+// It's possible to create and start a thread at the same time by using a static factory method (= a method that returns an object of a class)
+// Call start() on the thread and then return a reference to the thread. This way a thread can be created and started through a single method.
 
 public class Main {
 
@@ -380,19 +401,19 @@ public class Main {
         p2.t.start();
         c2.t.start();
 
-         **********************
-         Deadlock example
-         **********************
-         This example will create a deadlock, with RacingThread trying to call A.last() and MainThread trying to call B.last()
-         RacingThread owns the monitor on b, while it is waiting for the monitor on a
-         MainThread at the same time owns the monitor on a and is waiting to get b
+        // **********************
+        // Deadlock example
+        // **********************
+        // This example will create a deadlock, with RacingThread trying to call A.last() and MainThread trying to call B.last()
+        // RacingThread owns the monitor on b, while it is waiting for the monitor on a
+        // MainThread at the same time owns the monitor on a and is waiting to get b
         Deadlock dl = new Deadlock(); // this will trigger the Deadlock constructor, which will initiate the creation of 2 threads
         dl.deadLockStart();
 
-    // **********************
-    // Suspending and resuming a thread with use of a flag variable
-    // **********************
-    // This example illustrates how the wait() and notify() methods (inherited from Object) can be used to control the execution of a thread.
+        // **********************
+        // Suspending and resuming a thread with use of a flag variable
+        // **********************
+        // This example illustrates how the wait() and notify() methods (inherited from Object) can be used to control the execution of a thread.
 
         // Create the threads
         NewThread3 object1 = new NewThread3("One");
